a7c7b73c09145fea61fa9b3b7b242632515efd1d
facundothelabs <facundothelabs@gmail.com>
2024-11-13 13:13:13 -0300

    navbar, colores

--- front/klowhub/tailwind.config.ts
+++ front/klowhub/tailwind.config.ts
@@ -1,18 +1,55 @@
 import type { Config } from "tailwindcss";
+const {nextui} = require("@nextui-org/react");
+
 
 export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primario: {
+          100: '#dfd1f3',  // Morado muy claro
+          200: '#E5CCFF',
+          300: '#D6B3FF',
+          400: '#C799FF',
+          500: '#B880FF',
+          600: '#A966FF',
+          700: '#9A4DFF',
+          800: '#8B33FF',
+          900: '#7C1AFF',  // Morado oscuro
+        },
+        secundario: {
+          100: '#c0d6fb',  // Azul claro
+          200: '#a1c2fa',
+          300: '#81aef8',
+          400: '#6299f6',
+          500: '#4285F4',
+          600: '#4285F4',
+          700: '#0a49b0',
+          800: '#4285F4',
+          900: '#1A75FF',  // Azul oscuro
+        },
+        complemento: {          // Nuevo nombre para el segundo conjunto de morados
+          100: '#dfd1f3',  // Similar al primary pero podría tener variaciones
+          200: '#bfa3e7',
+          300: '#9f74dc',
+          400: '#9f74dc',
+          500: '#632daf',
+          600: '#532692',
+          700: '#421e75',
+          800: '#321758',
+          900: '#210f3a',
+        }
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 } satisfies Config;
