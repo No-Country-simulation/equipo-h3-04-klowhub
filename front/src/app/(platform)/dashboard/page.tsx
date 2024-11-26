fad4c87c5d2364a35736bf63ee2e60b1d051bdcd
SantiagoZarate <santiago2001zarate@gmail.com>
2024-11-26 12:28:26 -0300

    feat: add dashboard links

--- front/src/app/(platform)/dashboard/page.tsx
+++ front/src/app/(platform)/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import { Banner } from "./Banner";
+import { DashboardNav } from "./DashboardNav";
 
 export default function DashboardPage() {
   return (
-    <section className="flex flex-col p-14">
+    <section className="flex flex-col p-14 gap-12">
       <Banner />
+      <DashboardNav />
     </section>
   )
 }
