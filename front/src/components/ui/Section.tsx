121a3c8b60e3d9ae3d1c32017f6b6f7d93fd4e57
SantiagoZarate <santiago2001zarate@gmail.com>
2024-11-27 21:08:41 -0300

    style: add shadow to Section component

--- front/src/components/ui/Section.tsx
+++ front/src/components/ui/Section.tsx
@@ -5,6 +5,6 @@ type SectionProps = ComponentProps<"section">
 
 export function Section({ className, ...args }: SectionProps) {
   return (
-    <section className={cn("bg-white/10 flex flex-col gap-2 rounded-lg p-6", className)} {...args} />
+    <section className={cn("shadow-lg bg-white/10 flex flex-col gap-2 rounded-lg p-6", className)} {...args} />
   )
 }
