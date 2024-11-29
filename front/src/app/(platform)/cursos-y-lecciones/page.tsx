df6452023723dfa130a93717039ea7c71ba1ae47
SantiagoZarate <santiago2001zarate@gmail.com>
2024-11-29 16:15:29 -0300

    feat: add browse card para renderizar las cards en las paginas de busqueda

--- front/src/app/(platform)/cursos-y-lecciones/page.tsx
+++ front/src/app/(platform)/cursos-y-lecciones/page.tsx
@@ -1,7 +1,27 @@
-export default function CursosYLeccionesPage() {
+import { BrowseCard } from "@/components/cards/browse.card";
+import { courseService } from "@/services/course.service";
+
+export default async function CursosYLeccionesPage() {
+  const courses = await courseService({
+    relations: ['sectors', 'contentPillars', 'functionalities', 'platforms', 'platformsAndTool'],
+    take: 2
+  })
+
   return (
     <section>
-      CURSOS Y LECCIONES
+      <ul className="flex flex-col gap-6">
+        {
+          courses.map((course) =>
+            <BrowseCard
+              functionalities={course.functionalities.map(f => f.name)}
+              description={course.description}
+              image={course.image}
+              title={course.title}
+              key={course.id}
+            />
+          )
+        }
+      </ul>
     </section>
   )
 }
