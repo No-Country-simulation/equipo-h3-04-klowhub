7fe3fa63b94a25058775338b2f5f7e8efe63d894
SantiagoZarate <santiago2001zarate@gmail.com>
2024-11-29 16:06:44 -0300

    refactor: add chips list component

---
+++ front/src/components/ui/ChipsList.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils";
+import { Chip } from "@nextui-org/react";
+import { ComponentProps } from "react";
+
+interface Props extends ComponentProps<"ul"> {
+  items: string[];
+}
+
+export function ChipsList({ items, className, ...args }: Props) {
+  return (
+    <ul className={cn('flex flex-col gap-2 items-center', className)} {...args}>
+      {
+        items.map(item =>
+          <Chip className='bg-primario-100 text-primario-400 text-xs'>
+            {item}
+          </Chip>
+        )
+      }
+    </ul>
+  )
+}
