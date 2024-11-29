853b30898b191f21e43f94877559bad1f7719981
SantiagoZarate <santiago2001zarate@gmail.com>
2024-11-29 09:58:37 -0300

    feat: show dialog when completing create mentor profile course

--- front/src/app/(platform)/mentor/create-profile/education/page.tsx
+++ front/src/app/(platform)/mentor/create-profile/education/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { ConfirmationDialog } from "@/components/forms/ConfirmationDialog"
 import { educationSchema, EducationSchema } from "@/components/forms/createProfile/schemas/education"
 import { CreateProfileRichTextFields } from "@/components/forms/createProfile/schemas/type"
 import { ErrorMessage } from "@/components/forms/multiStepForm/ErrorMessage"
@@ -8,35 +9,40 @@ import { RichTextField } from "@/components/forms/multiStepForm/RichTextField"
 import { Button } from "@/components/ui/Button"
 import { Form, FormLabel } from "@/components/ui/form"
 import { zodResolver } from "@hookform/resolvers/zod"
+import { useDisclosure } from "@nextui-org/react"
 import { useForm } from "react-hook-form"
-import { toast } from "sonner"
 
 export default function EducationPage() {
   const form = useForm<EducationSchema>({
     resolver: zodResolver(educationSchema)
   })
 
+  const dialogState = useDisclosure()
+
   const handleSubmit = (data: EducationSchema) => {
     console.log({ data });
-    toast("PERFIL CREADO")
+    dialogState.onOpen()
   }
 
   return (
-    <form className="grid grid-cols-2 gap-6" onSubmit={form.handleSubmit(handleSubmit)}>
-      <p className="col-span-2">Detallá la formación académica y las certificaciones que avalen tu conocimiento.</p>
-      <Form {...form}>
-        <RichTextField<CreateProfileRichTextFields>
-          field="academicEducation"
-          label="Ingresá tu formación académica"
-          itemStyle="col-span-2"
-        />
-        <section className='flex flex-col gap-4'>
-          <FormLabel>Agregá certificaciones relevantes</FormLabel>
-          <FileField onDrop={(file) => form.setValue("certifications", file)} />
-          <ErrorMessage message={form.formState.errors.certifications?.message} />
-        </section>
-        <Button className="col-span-2" size="big" type="submit">Continuar</Button>
-      </Form>
-    </form>
+    <>
+      <ConfirmationDialog {...dialogState} />
+      <form className="grid grid-cols-2 gap-6" onSubmit={form.handleSubmit(handleSubmit)}>
+        <p className="col-span-2">Detallá la formación académica y las certificaciones que avalen tu conocimiento.</p>
+        <Form {...form}>
+          <RichTextField<CreateProfileRichTextFields>
+            field="academicEducation"
+            label="Ingresá tu formación académica"
+            itemStyle="col-span-2"
+          />
+          <section className='flex flex-col gap-4'>
+            <FormLabel>Agregá certificaciones relevantes</FormLabel>
+            <FileField onDrop={(file) => form.setValue("certifications", file)} />
+            <ErrorMessage message={form.formState.errors.certifications?.message} />
+          </section>
+          <Button className="col-span-2" size="big" type="submit">Continuar</Button>
+        </Form>
+      </form>
+    </>
   )
 }
