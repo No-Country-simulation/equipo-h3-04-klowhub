d53773db169b4ccda80b350c49b10a7aefc77ad0
SantiagoZarate <santiago2001zarate@gmail.com>
2024-11-28 21:39:29 -0300

    style: change link color and add more padding to them

--- front/src/components/navbar/navprincipal.tsx
+++ front/src/components/navbar/navprincipal.tsx
@@ -14,75 +14,70 @@ import SwitchHome from '../switch/switch.home';
 
 export default function NavBar() {
   return (
-    <>
-      <div className="w-full h-[93px]">
-        <Image
-          src="/navbar.png"
-          alt="Navbar background"
-          fill
-          priority
-          className="object-cover w-full h-full"
-        />
-        <Navbar maxWidth="full"
-          className="absolute top-[14px] h-[79px] z-10 bg-[rgba(31,32,38,0.6)]"
-        >
+    <div className="w-full h-[93px]">
+      <Image
+        src="/navbar.png"
+        alt="Navbar background"
+        fill
+        priority
+        className="object-cover w-full h-full"
+      />
+      <Navbar maxWidth="full"
+        className="absolute top-[24px] z-10 bg-[rgba(31,32,38,0.6)] flex justify-between gap-4"
+      >
+        <section className='flex items-center gap-6'>
           <NavbarBrand className='justify-between'>
-            <div className="relative w-32 h-32">
+            <div className="relative w-16 h-16">
               <Link href="/">
                 <Image
                   src="/logo.png"
                   alt="KlowHub Logo"
                   fill
                   style={{ transform: 'rotate(212.69deg)' }}
-                  className="object-contain"
+                  className="object-cover w-full h-full"
 
                 />
-
               </Link>
             </div>
             <SwitchHome />
             <div></div>
           </NavbarBrand>
-
-          <NavbarContent className="hidden lg:flex gap-4" justify="center">
-
+          <NavbarContent className="hidden lg:flex" justify="center">
             {navLinks.map(({ id, path, text }) => (
               <NavbarItem key={id}>
-
-                <Link className='text-sm lg:text-base xl:text-xl'
+                <Link className='text-sm text-primario-200 hover:text-primario-500 transition p-2'
                   href={path}>
-                  {text + " "}
+                  {text}
                 </Link>
-
               </NavbarItem>
             ))}
           </NavbarContent>
-          <NavbarContent justify="end">
-            <NavbarItem>
-              <ShoppingCart />
-            </NavbarItem>
-            <NavbarItem>
-              <Bell />
-            </NavbarItem>
-            <NavbarItem>
-              <Mail />
-            </NavbarItem>
+        </section>
+        <section className='flex items-center gap-6 '>
+          <NavbarItem>
+            <ShoppingCart />
+          </NavbarItem>
+          <NavbarItem>
+            <Bell />
+          </NavbarItem>
+          <NavbarItem>
+            <Mail />
+          </NavbarItem>
 
-            <NavbarItem className="hidden lg:flex mr-20">
-              <SwitchCreator />
-            </NavbarItem>
-            <NavbarItem>
-              <Image
-                alt="profile avatar"
-                className="object-cover rounded-switch w-full"
-                src="/avatar.png"
-                width={40}
-                height={40}
-              />
-            </NavbarItem>
-          </NavbarContent>
-        </Navbar>
-      </div>
-    </>
+          <NavbarItem className="hidden lg:flex">
+            <SwitchCreator />
+          </NavbarItem>
+          <NavbarItem className='relative'>
+            <Image
+              alt="profile avatar"
+              className="w-full h-full object-cover rounded-switch"
+              src="/avatar.png"
+              width={40}
+              height={40}
+            />
+          </NavbarItem>
+        </section>
+      </Navbar>
+    </div >
   );
 }
