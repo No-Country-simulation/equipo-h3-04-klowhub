6bfe975287a60577ff19ef6c2b58da6e3b110f91
SantiagoZarate <santiago2001zarate@gmail.com>
2024-12-09 12:13:33 -0300

    feat: add cart zustand store

---
+++ front/src/store/cart.store.ts
@@ -0,0 +1,27 @@
+import { Course } from '@/interfaces/course';
+import { create } from 'zustand';
+
+interface CartStore {
+  courses: Course[];
+}
+
+interface CartStoreActions {
+  addCourse: (course: Course) => void;
+  removeCourse: (id: Course['id']) => void;
+}
+
+export const useUserStore = create<CartStore & CartStoreActions>((set) => ({
+  courses: [],
+  addCourse(course) {
+    set((prevState) => ({
+      ...prevState,
+      courses: [...prevState.courses, course],
+    }));
+  },
+  removeCourse(id) {
+    set((prevState) => ({
+      ...prevState,
+      courses: prevState.courses.filter((course) => course.id !== id),
+    }));
+  },
+}));
