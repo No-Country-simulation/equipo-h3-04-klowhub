5f71c1a33d7098ae7e6f2b8cd203513a5efb7aac
SantiagoZarate <santiago2001zarate@gmail.com>
2024-12-09 11:08:57 -0300

    chore: add payment methods icons

---
+++ front/src/components/icons/ethereum-icon.tsx
@@ -0,0 +1,13 @@
+export function EthereumiIcon() {
+  return (
+    <svg width="51" height="46" viewBox="0 0 51 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <ellipse cx="25.6274" cy="23.0991" rx="25.0141" ry="22.6714" fill="#6481E7" />
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M25.9199 7.14844V30.0347L37.1403 24.0228L25.9199 7.14844Z" fill="#C1CCF5" />
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M14.6992 24.0228L25.9196 30.0347V7.14844L14.6992 24.0228Z" fill="white" />
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M25.9204 19.3984L14.6992 24.0204L25.9196 30.0322L37.1408 24.021L25.9204 19.3984Z" fill="#8299EC" />
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M25.9199 31.9597V40.2832L37.1478 25.9512L25.9199 31.9597Z" fill="#C1CCF5" />
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M25.9204 19.3984L14.6992 24.0204L25.9196 30.0322L25.9204 19.3984Z" fill="#C1CCF5" />
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M14.6992 25.9512L25.9196 40.2825V31.9597L14.6992 25.9512Z" fill="white" />
+    </svg>
+  )
+}
