debd22ecfc84b3d98c87ac1fff448375cddfc4f7
SantiagoZarate <santiago2001zarate@gmail.com>
2024-12-10 16:35:46 -0300

    fix: lazy load react video player

--- front/src/app/(platform)/dashboard/courses/vista-previa/_components/VistaPreviaMultimedia.tsx
+++ front/src/app/(platform)/dashboard/courses/vista-previa/_components/VistaPreviaMultimedia.tsx
@@ -1,13 +1,15 @@
-"use client"
-
 import { VideosGallery } from "@/components/multimedia/videos-gallery";
-import ReactPlayer from "react-player";
+import dynamic from "next/dynamic";
+
+const DynamicReactPlayer = dynamic(() => import("react-player").then(m => m.default), {
+  ssr: false,
+})
 
 export function VistaPreviaMultimedia() {
   return (
     <section>
       <figure className="aspect-video rounded-xl overflow-hidden shadow-2xl shadow-secundario-800">
-        <ReactPlayer
+        <DynamicReactPlayer
           url='https://youtu.be/rIrNIzy6U_g?si=ILDQ-uqvaEfm1L2s'
           controls={true}
           height={"100%"}
